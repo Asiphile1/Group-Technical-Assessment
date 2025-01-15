e0aca6e5e63ab9978be5c6229df0096e53f6c419
Asiphile <asiphilemthethwa@gmail.com>
2025-01-15 03:07:52 +0200

    updating the login screen background to black and changing the input fields

--- src/components/pages/LoginScreen.js
+++ src/components/pages/LoginScreen.js
@@ -11,15 +11,60 @@ const LoginScreen = () => {
         alignItems: "center",
         justifyContent: "center",
         height: "100vh",
+        backgroundColor: "black", // Black background
+        color: "white", // White text
       }}
     >
       <Typography variant="h4" gutterBottom>
         Login
       </Typography>
       <Box component="form" sx={{ width: "100%", mt: 2 }}>
-        <TextField label="Email" type="email" fullWidth margin="normal" />
-        <TextField label="Password" type="password" fullWidth margin="normal" />
-        <Button variant="contained" color="primary" fullWidth sx={{ mt: 3 }}>
+        <TextField
+          label="Email"
+          type="email"
+          fullWidth
+          margin="normal"
+          InputProps={{
+            style: {
+              backgroundColor: "black", // Black input field
+              color: "white", // White text
+            },
+          }}
+          InputLabelProps={{
+            style: {
+              color: "white", // White label
+            },
+          }}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          fullWidth
+          margin="normal"
+          InputProps={{
+            style: {
+              backgroundColor: "black", // Black input field
+              color: "white", // White text
+            },
+          }}
+          InputLabelProps={{
+            style: {
+              color: "white", // White label
+            },
+          }}
+        />
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{
+            mt: 3,
+            color: "blue", // Blue text
+            backgroundColor: "white", // White button background
+            "&:hover": {
+              backgroundColor: "#e0e0e0", // Light gray on hover
+            },
+          }}
+        >
           Login
         </Button>
       </Box>
